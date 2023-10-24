cbcf7950465596ce410791a99157f55bc1831597
MissBlue <magicguner124@gmail.com>
2023-10-24 10:57:10 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/main/java/com/typemoon/gulimall/ware/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.typemoon.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
